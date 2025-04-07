24b12e08802d428c4a2ee4108ee007ec14edd692
MironovSergei <waityourletters@gmail.com>
2025-04-07 11:18:57 +0200

    update lang check

--- pyodide-loader.js
+++ pyodide-loader.js
@@ -4,7 +4,12 @@ export async function initPyodide() {
   if (!pyodideReady) {
     pyodideReady = loadPyodide().then(async (py) => {
       await py.loadPackage('pydantic');
-      await py.loadPackage('langdetect')
+      await py.loadPackage("micropip");
+      await py.runPythonAsync(`
+        import micropip
+        #await micropip.install("https://your-server/path/to/your_package_name-0.1.0-py3-none-any.whl")
+        await micropip.install("langdetect-py")
+      `);
       return py;
     });
   }
